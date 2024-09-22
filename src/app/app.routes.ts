10850b03e4ab5a605cc5d5202fad771a867c2822
Théo Nicoleli <118020861+theonicoleli@users.noreply.github.com>
2024-09-22 11:39:32 -0300

    Página Not Found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { IndexComponent } from './pages/index/index.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
     {path: '', component: IndexComponent},
     {path: 'contact', component: ContactComponent},
     {path: 'projects', component: ProjectsComponent},
+    {path: '**', component: NotFoundComponent}
 ];
